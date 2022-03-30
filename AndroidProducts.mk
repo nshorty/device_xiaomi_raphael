1b1d3666ee2377c458cf1d7e1a980d3285220fd3
nshorty <andrysciortino@gmail.com>
2022-03-30 20:28:38 +0200

    raphael: crdroid bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_raphael.mk
+    $(LOCAL_DIR)/lineage_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_raphael-user \
-    nad_raphael-userdebug \
-    nad_raphael-eng
+    lineage_raphael-user \
+    lineage_raphael-userdebug \
+    lineage_raphael-eng
--- lineage_raphael.mk
+++ lineage_raphael.mk
@@ -9,21 +9,19 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := nad_raphael
+PRODUCT_NAME := lineage_raphael
 
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-NAD_BUILD_TYPE := OFFICIAL
-#USE_GAPPS ?= true
-#TARGET_GAPPS_ARCH := arm64
-USE_LAWNCHAIR := true
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/basic/config.mk)
 
 BUILD_FINGERPRINT := "google/redfin/redfin:12/SP2A.220305.012/8177914:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -33,5 +31,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 #USE_PIXEL_CHARGING := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
-EXTRA_FOD_ANIMATIONS := true
+TARGET_HAS_UDFPS := true
+EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTS_QUICK_TAP := true
